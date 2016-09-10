725abca79fc89483b21a991fce8feacfb9f4cd92
Arwid Thornstrom <arwidt@gmail.com>
2016-09-10 12:44:00 +0200

    updated

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ module.exports = function(from, to, async) {
     async = async ? true : false;
     return function(callback) {
         if(async) {
-            fse.copy(from, to, callback);
+            fse.copy(from, to, function(err) {
+                if (err) return console.error(err);
+                callback();
+            });
         } else {
             fse.copySync(from, to);
             callback();
